52c889e9ed3427fd6058232acc50ec948287c50a
Lucas Lopes <Lucas.santos.pessoal@outlook.com>
2022-03-01 13:38:14 -0300

    Creating form with input and button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from './To_do_form';
+import Item from './item';
 
 const Card = (props) => {
+    const [items, setItems] = useState([]);
 
+    function onAddItem(text){
+        let newItem = new Item(text);
+        setItems([...items, newItem]);
+    };
 
     return(
         <div className='card'>
             <h1 className='principal-text'>To-Do List</h1>
-            
+            <Form onAddItem={onAddItem}/>
         </div>
     );
 
