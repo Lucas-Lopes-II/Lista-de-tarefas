aa496cc0d58430759a85e870970042a228cf389e
Lucas Lopes <Lucas.santos.pessoal@outlook.com>
2022-03-03 10:58:12 -0300

    Creating error message

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import Form from './To_do_form';
 import List from './List';
+import Error from './ErrorMsg'
 import Item from './item';
 
 const Card = (props) => {
     const [items, setItems] = useState([]);
+    const [errorMsg, setError] = useState(false);
 
     function onAddItem(text){
         let newItem = new Item(text);
         setItems([...items, newItem]);
+        setError(false);
     };
 
     return(
@@ -16,6 +19,7 @@ const Card = (props) => {
             <h1 className='principal-text'>To-Do List</h1>
             <Form onAddItem={onAddItem}/>
             <List items={items}/>
+            <Error show={errorMsg}/>
         </div>
     );
 
